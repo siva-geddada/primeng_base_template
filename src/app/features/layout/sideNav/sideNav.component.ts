f83382482d20d3be0082492f6a6909065b8c6a7b
Siva_Geddada <siva.cs579@gmail.com>
2025-05-30 22:48:26 +0530

    added sidenav component

---
+++ src/app/features/layout/sideNav/sideNav.component.ts
@@ -0,0 +1,9 @@
+import { ChangeDetectionStrategy, Component } from '@angular/core';
+
+@Component({
+  selector: 'app-side-nav',
+  imports: [],
+  templateUrl: './sideNav.component.html',
+  styleUrls: ['./sideNav.component.scss'],
+})
+export class SideNavComponent { }
